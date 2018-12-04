ae99d7a78bfe96473e860231e38e38ccb876bbda
k1ic <chenkaill@hotmail.com>
2018-12-04 15:19:58 +0800

    Create mc.py

---
+++ mc.py
@@ -0,0 +1,26 @@
+import bmemcached
+import time
+import random
+
+client = bmemcached.Client(('10.235.65.18:11211'), 'bmemcached', 'bmemcached')
+
+i=0
+while(i<10000):
+    print(i)
+
+    t = time.time()
+    #print(type(t), t)
+    s = lambda:int(round(t * 1000000))
+    s = s()
+    print(type(s), s)
+
+    k = 'ck_t_mc_' + str(s)
+    print(type(k), k)
+
+    res_set = client.set(k, s, 86400)
+    print(res_set)
+
+    res_get = client.get(k)
+    print(res_get)
+
+    i+=1;
